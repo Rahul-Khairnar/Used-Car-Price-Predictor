c8cc243a3f6fe457a223108a1288a341050c3a1c
Rahul-Khairnar <rahulkhairnar10e@gmail.com>
2020-08-10 13:03:58 +0530

    Adding the model building code. 99% complete.

--- Model.py
+++ Model.py
@@ -7,28 +7,15 @@ Created on Sat Aug  8 22:15:36 2020
 
 
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import ShuffleSplit
-from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LinearRegression
-from sklearn.svm import SVC
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import Lasso
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import GridSearchCV
-from sklearn.pipeline import Pipeline
-
-from sklearn import linear_model
-from sklearn import svm
-
-
-
 
 
 df = pd.read_csv("Cleaned_data_final.csv")
-df.isnull().sum()
+cars = df["Name"].unique().tolist()
+df["Brand"] = df["Name"].apply(lambda x:x.split(" ")[0])
 df3 = df.dropna()
 df3["Age"] = df3["Year"].apply(lambda x:2020-x)
 df1= df3.copy()
@@ -44,7 +31,6 @@ X = df_dummy_dupli.drop("Price",axis = 1)
 
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=10)   
 
-
 linreg = LinearRegression()
 linreg.fit(X_train, y_train) 
 linreg.score(X_test,y_test)
@@ -53,9 +39,33 @@ ranreg = RandomForestRegressor(n_estimators = 100,criterion="mse")
 ranreg.fit(X_train,y_train)
 ranreg.score(X_test,y_test)
 
-x = [40000,20.1,1248,88.5,5,3,1,0,0,0,1,1,0]
-ranreg.predict([x])
+def price_predict(kilometers,mileage,engine,power,seats,age,fuel,transmission,owner,brand):
+    columns = X_train.columns.tolist()
+    print(type(columns))
+    x = np.zeros(len(columns))
+    x[0] = kilometers
+    x[1] = mileage
+    x[2] = engine
+    x[3] = power
+    x[4] = seats
+    x[5] = age
+    for d in columns:
+        if fuel in d:
+            pos = columns.index(d)
+            x[pos] = 1
+        if transmission in d:
+            pos = columns.index(d)
+            x[pos] = 1
+        if owner in d:
+            pos = columns.index(d)
+            x[pos] = 1
+        if brand in d:
+            pos = columns.index(d)
+            x[pos] = 1
+    return ranreg.predict([x])[0]
 
+list1 = price_predict(78000,14.74,2993,270.9,5,9,"Diesel","Automatic","Second","Jaguar")
+print(round(list1,2))
 
 
 
